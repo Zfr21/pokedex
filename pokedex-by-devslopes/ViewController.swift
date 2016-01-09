1450fd55d9f8c4400eb2d42b757a022217ad40b8
ZAFER CELALOGLU <zafer.celaloglu@gmail.com>
2016-01-09 11:18:24 +0200

    Initial Commit

---
+++ pokedex-by-devslopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-devslopes
+//
+//  Created by Zafer Celaloglu on 9.01.2016.
+//  Copyright © 2016 Zafer Celaloglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
