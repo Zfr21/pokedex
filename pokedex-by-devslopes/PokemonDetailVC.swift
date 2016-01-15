af637326de36fa869dc44dd14726f976f0f61260
ZAFER CELALOGLU <zafer.celaloglu@gmail.com>
2016-01-15 20:11:03 +0200

    completed json parsing and updated UI with Pokemon model class

--- pokedex-by-devslopes/PokemonDetailVC.swift
+++ pokedex-by-devslopes/PokemonDetailVC.swift
@@ -20,20 +20,49 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var nextEvoImg: UIImageView!
     @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttack: UILabel!
 
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       nameLbl.text = pokemon.name
-       mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        nameLbl.text = pokemon.name
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        currentEvoImg.image = img
 
-       pokemon.downloadPokemonDetails { () -> () in
+
+        pokemon.downloadPokemonDetails { () -> () in
             //this will be called after download is done
+            self.updateUI()
         }
     }
-    
+
+    func updateUI() {
+        descriptionLbl.text = pokemon.description
+        typeLbl.text = pokemon.type
+        defenceLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        weightLbl.text = pokemon.weight
+        baseAttack.text = pokemon.attack
+
+        if pokemon.nextEvolutionId == "" {
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.hidden = true
+        } else {
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutionTxt)"
+
+            if pokemon.nextEvolutionLvl != "" {
+                str += " - LVL \(pokemon.nextEvolutionLvl)"
+            }
+        }
+
+    }
     @IBAction func backBtnPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
